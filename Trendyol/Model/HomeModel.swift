01f7efd3e128bd9127d4360b46377b87ec74c563
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-11-05 09:48:27 +0300

    MVVM - Architectural Pattern

---
+++ Trendyol/Model/HomeModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModel.swift
+//  Trendyol
+//
+//  Created by IREM SEVER on 5.11.2024.
+//
+
+import Foundation
---
+++ Trendyol/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Trendyol
+//
+//  Created by IREM SEVER on 5.11.2024.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
