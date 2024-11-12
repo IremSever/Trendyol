7e59b04ed98f2ecdc82d435866da785fa4f4d002
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-11-12 15:02:49 +0300

    update

---
+++ Trendyol/View/Cell/Promotion/Promotion Item/PromotionItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  PromotionItemCell.swift
+//  Trendyol
+//
+//  Created by IREM SEVER on 12.11.2024.
+//
+
+import UIKit
+
+class PromotionItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Trendyol/View/Cell/Promotion/PromotionCell.swift
+++ Trendyol/View/Cell/Promotion/PromotionCell.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class PromotionCell: UICollectionViewCell {
-
+class PromotionCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
+    private var viewModel = HomeViewModel()
+    @IBOutlet weak var collectionViewProduct: UICollectionView!
+    @IBOutlet weak var lblSeconds: UILabel!
+    @IBOutlet weak var lblMinute: UILabel!
+    @IBOutlet weak var lblHour: UILabel!
+    @IBOutlet weak var viewStackTimer: UIStackView!
+    @IBOutlet weak var collectionViewCategory: UICollectionView!
+    @IBOutlet weak var viewBg: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        loadData()
+    }
+    
+    func loadData() {
+        collectionViewProduct.dataSource = self
+        collectionViewProduct.delegate = self
+        
+        collectionViewCategory.dataSource = self
+        collectionViewCategory.delegate = self
     }
 
+    func registerCell() {
+        collectionViewCategory.register(UINib(nibName: "CategoryCell", bundle: nil), forCellWithReuseIdentifier: "CategoryCell")
+        
+        collectionViewProduct.register(UINib(nibName: "ProductCell", bundle: nil), forCellWithReuseIdentifier: "ProductCell")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.homeData?.items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 }
